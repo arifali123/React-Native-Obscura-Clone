19e379ded8b466c5d60115b068ba97ea7144eb6a
Arif Ali <51419655+arifali123@users.noreply.github.com>
2025-10-02 21:30:09 -0700

    upgrades

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,12 @@ import {
   StyleSheet,
   Platform,
   View,
-  SafeAreaView,
   StatusBar,
   TouchableHighlight,
   Linking,
   Text,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import {
   Camera,
   useCameraDevice,
